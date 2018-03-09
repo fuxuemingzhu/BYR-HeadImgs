9455cd3e5981e38a35a312e2a804992594d4a15b
fugui cai <fuxuemingzhu@163.com>
2018-03-09 10:18:28 +0800

    更新图片

--- bbsspider/spiders/const.py
+++ bbsspider/spiders/const.py
@@ -6,5 +6,3 @@ HEADERS = {'User-Agent': 'Mozilla/5.0', 'Host': 'bbs.byr.cn', 'X-Requested-With'
            'Connection': 'keep-alive'}
 # 请修改为你自己的用户名id和密码passwd，其他不用修改，提交代码的时候记得抹掉密码 =. =
 LOGIN_DATA = {'id': 'fuxuemingzhu', 'passwd': '*********', 'mode': '0', 'CookieDate': '0'}
-DB_CONFIG = {'host': '127.0.0.1', 'user': 'root', 'passwd': '123456', 'db': 'bbs', 'port': 3306, 'charset': 'utf8',
-             'timeout': 5}
